9a6b2f0833584b7cfe24c51a1bd8246ff2fbbceb
martadossantos <68651278+martadossantos@users.noreply.github.com>
2026-09-05 16:40:36 +0100

    encode text function to test
    encode text function in embeddings.py to test on a couple of products first

--- embeddings.py
+++ embeddings.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import numpy as np
+import pandas as pd
 
 # Pad the images so that they're squares
 # To then use in the image embedder
@@ -49,5 +51,36 @@ def build_text_feature(df):
     return df[['external_code', 'text']]
 
 # Encode text
-def encode_text():
-    return
+def encode_text(df, model, processor, device, batch_size):
+    texts = df['text'].tolist()
+    codes = df['external_code'].tolist()
+
+    # list to hold all the new vectors
+    all_features = []
+
+    # process the texts in batches
+    for n in range(0, len(texts), batch_size):
+        batch = texts[n:n + batch_size]
+        inputs = processor(text=batch, padding=True, truncation=True, return_tensors='pt')
+
+        inputs = inputs.to(device)
+
+        # to return the 768 embedding
+        with torch.no_grad():
+            features = model.get_text_features(**inputs)
+
+        # check feature shape is correct
+        assert features.shape[1] == 768
+    
+        features = features.cpu().numpy()
+        all_features.append(features)
+
+    # stack features vertically    
+    matrix = np.vstack(all_features)
+
+    # build df to return
+    columns = [f'text_{i}' for i in range(768)]
+    encoded_df = pd.DataFrame(matrix, columns=columns)
+    encoded_df.insert(0, 'external_code', codes)
+
+    return encoded_df
