ff1617a91a2af990c51d7d92b5875305c6f07c72
martadossantos <68651278+martadossantos@users.noreply.github.com>
2026-09-05 23:04:30 +0100

    encode images
    - encode img function in embeddings
    - quick check that it's encoding with 10 prods
    - encode all train and test images and create parquet file

--- embeddings.py
+++ embeddings.py
@@ -26,8 +26,44 @@ def make_square(img, fill=(255, 255, 255)):
 
 
 # Encode images
-def encode_images():
-    return
+def encode_images(df, model, processor, device, batch_size, image_root):
+    img_paths = df['image_path'].tolist()
+    codes = df['external_code'].tolist()
+
+    # list to hold all the new vectors
+    all_features = []
+
+    # process the images in batches
+    for n in range(0, len(img_paths), batch_size):
+        batch_paths = img_paths[n:n + batch_size]
+
+        # open and square each image in the batch
+        batch_images = []
+
+        for p in batch_paths:
+            img = Image.open(image_root / p)
+            batch_images.append(make_square(img))
+
+        inputs = processor(images=batch_images, return_tensors='pt')
+        inputs = inputs.to(device)
+
+        # to return the 768 embedding
+        with torch.no_grad():
+            features = model.get_image_features(**inputs).pooler_output
+    
+        features = features.cpu().numpy()
+        all_features.append(features)
+
+    # stack features vertically    
+    matrix = np.vstack(all_features)
+
+    # build df to return
+    columns = [f'image_{i}' for i in range(768)]
+    encoded_df = pd.DataFrame(matrix, columns=columns)
+    encoded_df.insert(0, 'external_code', codes)
+
+    return encoded_df
+
 
 # Construct text feature from the product's metadata
 def build_text_feature(df):
