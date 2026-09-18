9b04843d34fff9741ece7dd880178a3280f60577
martadossantos <68651278+martadossantos@users.noreply.github.com>
2026-09-18 19:33:56 +0100

    condition model class
    beginning of condition model class

--- models.py
+++ models.py
@@ -75,3 +75,18 @@ print(head)
 
 
 # condition model
+
+# example from PyTorch docs
+# class Model(nn.Module):
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(1, 20, 5)
+#         self.conv2 = nn.Conv2d(20, 20, 5)
+
+#     def forward(self, x):
+#         x = F.relu(self.conv1(x))
+#         return F.relu(self.conv2(x))
+
+
+# class ConditionModel(nn.Module):
+#     return self
